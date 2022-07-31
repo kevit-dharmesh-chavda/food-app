202b7641853f520e3fc82921d03f2614b37acbee
dharesh2712 <19comp.dharmesh.chavda@gmail.com>
2022-08-01 00:58:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/recipes',pathMatch:'full'},
+  {path:'recipes',component:RecipesComponent},
+  {path : 'shoppinglist', component:ShoppinglistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
